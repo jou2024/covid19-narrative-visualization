8907d73026b9ceff1dcdab8b53d5971a87493a89
jou2024 <162074942+jou2024@users.noreply.github.com>
2024-07-27 22:53:29 -0400

    backup preprocess

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,26 +1,27 @@
 import pandas as pd
 
 # Load the data
-data = pd.read_csv('../data/us-states.csv')
+data = pd.read_csv('data/us-states.csv')
 
 # Convert date column to datetime format
 data['date'] = pd.to_datetime(data['date'])
 
+year = '2020'
 # Filter data for the year 2022
-data_2022 = data[(data['date'] >= '2022-01-01') & (data['date'] <= '2022-12-31')]
+data_year = data[(data['date'] >= (year +'-01-01')) & (data['date'] <= (year + '-12-31'))]
 
 # Calculate total deaths by state for 2022
-total_deaths_state_2022 = data_2022.groupby('state')['deaths'].sum().reset_index()
-total_deaths_state_2022.columns = ['state', 'total_deaths_2022']
+total_deaths_state_year = data_year.groupby('state')['deaths'].sum().reset_index()
+total_deaths_state_year.columns = ['state', 'total_deaths_'+year]
 
 # Calculate monthly deaths by state for 2022
-data_2022['month'] = data_2022['date'].dt.to_period('M')
-monthly_deaths_state_2022 = data_2022.groupby(['state', 'month'])['deaths'].sum().reset_index()
-monthly_deaths_state_2022.columns = ['state', 'month', 'monthly_deaths_2022']
-monthly_deaths_state_2022['month'] = monthly_deaths_state_2022['month'].astype(str)
+data_year['month'] = data_year['date'].dt.to_period('M')
+monthly_deaths_state_year = data_year.groupby(['state', 'month'])['deaths'].sum().reset_index()
+monthly_deaths_state_year.columns = ['state', 'month', 'monthly_deaths_'+year]
+monthly_deaths_state_year['month'] = monthly_deaths_state_year['month'].astype(str)
 
 # Save the cleaned data to new CSV files
-total_deaths_state_2022.to_csv('../data/total_deaths_state_2022.csv', index=False)
-monthly_deaths_state_2022.to_csv('../data/monthly_deaths_state_2022.csv', index=False)
+total_deaths_state_year.to_csv('data/total_deaths_state_'+year+'.csv', index=False)
+monthly_deaths_state_year.to_csv('data/monthly_deaths_state_'+year+'.csv', index=False)
 
 print("Data cleaning and preprocessing completed.")
